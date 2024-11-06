9133af5ae7a0802ee18d35317d90577c5b83aa69
Reinier The Ghost <reinierrolbrouwer@gmail.com>
2024-11-06 22:25:20 +0100

    added 4 mahogany, 4 kapok and 4 leopard trees from nbt

--- src/main/java/com/legends/edumia/client/render/model/connectedtex/ConnectedTexture2DContext.java
+++ src/main/java/com/legends/edumia/client/render/model/connectedtex/ConnectedTexture2DContext.java
@@ -1,4 +1,31 @@
 package com.legends.edumia.client.render.model.connectedtex;
 
+import java.util.Collection;
+import java.util.EnumSet;
+
 public class ConnectedTexture2DContext {
+
+    private final EnumSet<RelativePosition> relativePositions;
+
+    public ConnectedTexture2DContext(Collection<RelativePosition> positions){
+        this.relativePositions = EnumSet.copyOf(positions);
+    }
+
+
+
+    public static enum RelativePosition {
+        TOP_LEFT,
+        TOP,
+        TOP_RIGHT,
+        LEFT,
+        CENTRE,
+        RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM,
+        BOTTOM_RIGHT;
+
+        private RelativePosition(){
+
+        }
+    }
 }
